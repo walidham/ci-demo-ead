550f0f755017c2ffae2879adc51c247fb46c41ca
walid hammami <walid.ham@hotmail.com>
2026-02-14 14:37:56 +0100

    Create Github action with azure VM

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ app.get("/", (req, res) => {
   res.send("Hello World - CI/CD with GitHub Actions + Docker! JOO ");
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 app.listen(port, () => console.log(`Server running on port ${port}`));
