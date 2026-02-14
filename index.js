57aa12a10c75cc7762ba50b410efcc540f1cd2d3
walid hammami <walid.ham@hotmail.com>
2026-02-14 14:43:51 +0100

    DSI Fedda ella ya9rrou fi gafsa

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World - CI/CD with GitHub Actions + Docker! JOO ");
+  res.send("Hello World - CI/CD with GitHub Actions DSI FEDDDa ");
 });
 
 const port = process.env.PORT || 80;
